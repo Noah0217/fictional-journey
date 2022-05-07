6185f8dae4b14dc9bd4823aa6672d128a9f6f746
Noah <noahm0217@gmail.com>
2022-05-06 18:10:02 -0600

    test array added

--- test.js
+++ test.js
@@ -1,7 +1,8 @@
 let [a,b,...e] = [1,2,3,4,5]
 console.log(e)
 
-
-
 const myArr = ["Orange", "Banana", "Mango", "Kiwi"];
 console.log(myArr);
+
+const Arr = ["123", "1234", "12345", "123456"];
+console.log(Arr);
